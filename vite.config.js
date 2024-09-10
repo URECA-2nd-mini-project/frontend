7a3acddd7f53c4c413c739691e20f3206e17fb8d
shinhm1 <shinhm1@naver.com>
2024-09-10 14:52:28 +0900

    feat: 개발 환경 세팅

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 서버 통신 프록시 주소 설정
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  // 전역 CSS 설정
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/_common.scss";`,
+      },
+    },
+  },
+});
