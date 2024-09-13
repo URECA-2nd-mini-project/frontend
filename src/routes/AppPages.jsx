798576eba2d8a8b28d7e34f5988fcb39759e1751
shinhm1 <shinhm1@naver.com>
2024-09-13 18:02:21 +0900

    feat: Navigation bar UI 개발 완료, SVGR 이용한 SVG import 추가

--- src/routes/AppPages.jsx
+++ src/routes/AppPages.jsx
@@ -1,21 +1,40 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRouteDef } from './RouteDef';
 // import Header from '../layouts/Header';
-// import NavMenu from '../layouts/NavMenu';
+import NavMenu from '../layouts/NavMenu';
 // import PlayBar from '../layouts/PlayBar';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Contents = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
 const AppPages = () => {
   return (
     <BrowserRouter>
-      {/* <NavMenu /> */}
-      {/* <Header /> */}
-      <Routes>
-        {/* RouteDef에 정의된 경로를 불러와 현재 경로에 맞는 화면을 보여줌 */}
-        {Object.entries({ ...AppRouteDef }).map(([name, { path, element }], index) => (
-          <Route key={name + index} path={path} element={element} />
-        ))}
-      </Routes>
-      {/* <PlayBar /> */}
+      <Wrapper>
+        <NavMenu />
+        <Contents>
+          {/* <Header /> */}
+          <Routes>
+            {/* RouteDef에 정의된 경로를 불러와 현재 경로에 맞는 화면을 보여줌 */}
+            {Object.entries({ ...AppRouteDef }).map(([name, { path, element }], index) => (
+              <Route key={name + index} path={path} element={element} />
+            ))}
+          </Routes>
+          {/* <PlayBar /> */}
+        </Contents>
+      </Wrapper>
     </BrowserRouter>
   );
 };
